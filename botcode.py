67dc1903e4d6dea69710af47864d204a4633f1b4
VashitvaR <vashitva.bagga@gmail.com>
2024-02-22 01:45:43 +0530

    cc

--- botcode.py
+++ botcode.py
@@ -260,6 +260,7 @@ def main():
             response = "Select a valid method."
 
         st.text_area("Nayak's Response:", response)
-
+    if response is None:
+        response = "I'm sorry, I didn't understand that. Please try asking in a different way or provide more details."
 if __name__ == "__main__":
     main()
